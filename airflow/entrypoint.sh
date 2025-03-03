6f72c5e71ba98e45d430588bda97d5ad0c1a0a65
Kyungbin Choi <iris122413@gmail.com>
2025-03-04 08:22:08 +0900

    setting envs

---
+++ airflow/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# 데이터베이스 초기화
+airflow db init
+
+# 기본 사용자 생성
+airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@example.com \
+    --password admin123
+
+# 전달된 명령 실행
+exec airflow webserver -p 8080 & airflow scheduler
+
